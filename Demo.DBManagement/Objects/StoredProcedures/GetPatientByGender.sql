fe7955c36c36040a73ca2c09e54c99bc86426446
Jeriffe Jiang <jjiang@touchpointmed.com>
2024-05-27 09:03:19 +0800

    Updated

---
+++ Demo.DBManagement/Objects/StoredProcedures/GetPatientByGender.sql
@@ -0,0 +1,24 @@
+﻿CREATE  PROCEDURE [dbo].[GetPatientByGender] 
+@Gender CHAR(1),
+@Offset INT,
+@PageSize INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+	     P.PatientID
+		,P.FirstName
+		,P.MiddleInitial
+		,P.LastName
+		,P.BirthDate
+		,P.Gender
+		,P.MedRecNumber
+		,P.PatientType
+		,P.SiteID
+	FROM dbo.T_PATIENT AS P
+	WHERE Gender = @Gender
+	ORDER BY PatientID
+	OFFSET @Offset ROWS
+	FETCH NEXT @PageSize ROWS ONLY
+END
---
+++ Demo.DBManagement/Objects/StoredProcedures/GetPatientByMedRecNumber.sql
@@ -0,0 +1,26 @@
+﻿CREATE  PROCEDURE [dbo].[GetPatientByMedRecNumber] 
+@MedRecNumber INT,
+@Offset INT,
+@PageSize INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+	     P.PatientID
+		,P.FirstName
+		,P.MiddleInitial
+		,P.LastName
+		,P.BirthDate
+		,P.Gender
+		,P.MedRecNumber
+		,P.PatientType
+		,P.SiteID
+	FROM dbo.T_PATIENT AS P
+	INNER JOIN dbo.T_VISIT AS V 
+		ON P.PatientID = V.PatientID
+	WHERE V.CareUnitID = @MedRecNumber
+	ORDER BY PatientID
+	OFFSET @Offset ROWS
+	FETCH NEXT @PageSize ROWS ONLY
+END
